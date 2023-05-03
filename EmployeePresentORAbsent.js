61e83539e9c7f6c17445fc2cfa61b06e64bd6deb
Ritvik5 <sharma.ritvik56@gmail.com>
2023-05-03 17:06:48 +0530

    Checking Employee is Present or Absent

--- EmployeePresentORAbsent.js
+++ EmployeePresentORAbsent.js
@@ -1,1 +1,15 @@
 // Welcome to Employee Payroll Management
+
+//UC1-Employee is present or absent.
+
+
+const IS_ABSENT=0;
+let empCheck = Math.floor(Math.random()*10)%2;
+if(empCheck==IS_ABSENT)
+{
+    console.log("Employee is absent");
+}
+else
+{
+    console.log("Employee is present");
+}
