d4ee17476c676c6605edb873a3e768676c017901
Ritvik5 <sharma.ritvik56@gmail.com>
2023-05-03 17:43:37 +0530

    Calculating Employee Monthly Wage

--- EmployeeManagement.js
+++ EmployeeManagement.js
@@ -5,6 +5,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS=20;
 
 /*let empHrs=0;
 let empCheck=Math.floor(Math.random()* 10 % 3);
@@ -37,9 +38,24 @@ function getWorkingHours(empCheck)
           return 0;
   }
 }
-let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
-empHrs=getWorkingHours(empCheck);
-let empWage=empHrs*WAGE_PER_HOUR;
-console.log("Employee Hour: "+empHrs+" Emp Wage: "+empWage);
+
+/*{
+    let empHrs=0;
+    let empCheck=Math.floor(Math.random()*10)%3;
+    empHrs=getWorkingHours(empCheck);
+    let empWage=empHrs*WAGE_PER_HOUR;
+    console.log("Employee Hour: "+empHrs+" Emp Wage: "+empWage);
+}*/
+
+// UC4-Calculating Monthly Wage
+
+let totalEmpHrs=0;
+ for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
+ {
+   let empCheck=Math.floor(Math.random()*10)%3;
+   totalEmpHrs+=getWorkingHours(empCheck);
+ }
+
+ let empWage=totalEmpHrs*WAGE_PER_HOUR;
+ console.log("Working Hour: "+totalEmpHrs+" Emp Monthly Wage: "+empWage);
 
