af03edc25084aa57794cc1e836f0d0e1e1d66a0b
Ritvik5 <sharma.ritvik56@gmail.com>
2023-05-03 18:15:21 +0530

    Calculating wage till a Condition

--- EmployeeManagement.js
+++ EmployeeManagement.js
@@ -49,6 +49,7 @@ function getWorkingHours(empCheck)
 
 // UC4-Calculating Monthly Wage
 
+/*{
 let totalEmpHrs=0;
  for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
  {
@@ -58,4 +59,24 @@ let totalEmpHrs=0;
 
  let empWage=totalEmpHrs*WAGE_PER_HOUR;
  console.log("Working Hour: "+totalEmpHrs+" Emp Monthly Wage: "+empWage);
+}*/
 
+// UC-5 -Calculate Wages till a condition of working hours of 160 or max days of 20 is reached for a month
+
+{
+    const MAX_HRS_IN_MONTH=160;
+    const NUM_OF_WORKING_DAYS=20;
+    let totalEmpHrs=0;
+    
+    let totalWorkingDays=0;
+    while(totalEmpHrs <= MAX_HRS_IN_MONTH && 
+          totalWorkingDays < NUM_OF_WORKING_DAYS)
+    {
+       totalWorkingDays++;
+       let empCheck = Math.floor(Math.random()*10)%3;
+       totalEmpHrs+=getWorkingHours(empCheck);
+    }
+   let empWage = totalEmpHrs*WAGE_PER_HOUR;
+   console.log("Total Days: "+totalWorkingDays+
+   " Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage); 
+}
