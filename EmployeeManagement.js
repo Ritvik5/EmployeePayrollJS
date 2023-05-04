bb37398d0406721ad63decdff9692e0673fe53ed
Ritvik5 <sharma.ritvik56@gmail.com>
2023-05-04 16:54:27 +0530

    Storing Dail Wage in an Array

--- EmployeeManagement.js
+++ EmployeeManagement.js
@@ -6,6 +6,8 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
 const NUM_OF_WORKING_DAYS=20;
+const MAX_HRS_IN_MONTH=160;
+
 
 /*let empHrs=0;
 let empCheck=Math.floor(Math.random()* 10 % 3);
@@ -63,7 +65,7 @@ let totalEmpHrs=0;
 
 // UC-5 -Calculate Wages till a condition of working hours of 160 or max days of 20 is reached for a month
 
-{
+/*{
     const MAX_HRS_IN_MONTH=160;
     const NUM_OF_WORKING_DAYS=20;
     let totalEmpHrs=0;
@@ -79,4 +81,28 @@ let totalEmpHrs=0;
    let empWage = totalEmpHrs*WAGE_PER_HOUR;
    console.log("Total Days: "+totalWorkingDays+
    " Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage); 
+}*/
+
+// UC6-Saving The Daily Wage in an Array
+
+{
+    let totalEmpHrs=0;
+    let totalWorkingDays=0;
+    let empDailyWageArr=new Array();
+    function calcDailyWage(empHrs)
+    {
+        return empHrs*WAGE_PER_HOUR;
+    }
+    while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+    {
+        totalWorkingDays++;
+    
+        let empCheck=Math.floor(Math.random()*10)%3;
+        let empHrs=getWorkingHours(empCheck);
+        totalEmpHrs+=empHrs;
+        empDailyWageArr.push(calcDailyWage(empHrs));
+    }
+
+        let empWage=calcDailyWage(totalEmpHrs);
+        console.log("Total Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
 }
