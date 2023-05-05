52d5cb933886b6907baa5daa2e7c6d9e71358a31
Ritvik5 <sharma.ritvik56@gmail.com>
2023-05-05 06:14:37 +0530

    Using aaray some function to fins is any Part time Wage

--- ArrayHelper.js
+++ ArrayHelper.js
@@ -95,4 +95,11 @@ function isAllFulltimeWage(dailyWage)
     return dailyWage.includes("160");
 }
 
-console.log("Check All Elements have Full Time Wage: "+fullDayWageArr.every(isAllFulltimeWage));
+// console.log("Check All Elements have Full Time Wage: "+fullDayWageArr.every(isAllFulltimeWage));
+
+//UC 7F - Check if there is any Part Time Wage
+function isAnyPartTimeWage(dailyWage)
+{
+   return dailyWage.includes("80");
+}
+console.log("Check If Any Part Time Wage: "+mapDayWithWageArr.some(isAnyPartTimeWage));
